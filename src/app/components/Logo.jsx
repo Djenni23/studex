fab1ded02c6da72884a6ca3119ec31a042e74474
Djenni23 <djenebadiallo2309@gmail.com>
2024-05-19 23:04:22 +0300

    added img

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Logo() {
   return (
     <div className='text-3xl font-bold text-purple-900 '>
-      Studex
+      <Image src={"/studexLogo.png"} width={150} height={20}></Image>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,25 @@
+"use client"
 import React from 'react'
+import Logo from './components/Logo'
+import { useRouter } from 'next/navigation'
 
 function Home() {
+  const router = useRouter()
   return (
-    <div>
-      
+    <div className='h-screen flex flex-col
+    items-center justify-center
+    bg-neutral-100' >
+      <Logo></Logo>
+      <div>
+      <div className='mt-20'>
+        <div className='flex gap-5'>
+         <div onClick={()=>{router.push("/signIn")}} className='cursor-pointer h-20 w-24 text-white bg-purple-900 flex
+         justify-center items-center rounded'>Sign In</div>
+         <div onClick={()=>{router.push("/signUp")}} className='cursor-pointer h-20 w-24 text-white bg-purple-900 flex
+         justify-center items-center rounded'>Sign Up</div>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
